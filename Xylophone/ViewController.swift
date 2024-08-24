a101b608d89e5e90b89f746bd9d7ba464131408c
Mayank08082004 <mayankc1533@gmail.com>
2024-08-24 07:38:10 +0530

    first commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        playSound(soundName: sender.currentTitle!)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            // Put your code which should be executed with a delay here
+            sender.alpha = 1
+        }
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
